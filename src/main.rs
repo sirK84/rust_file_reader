4537f54ac40e2fe5d8b86a25ed6d0f45bbac6245
Krzysztof Dudkiewicz <krzysztof.dudkiewicz@nokia.com>
2025-08-04 15:27:26 +0200

    rust lab 1

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("No filename provided. Usage: {} <filename>", args[0]);
+        std::process::exit(1);
+    }
+    let filename = args[1].clone();
+    let file = File::open(&filename);
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            std::io::ErrorKind::NotFound => {
+                panic!("File {} not found: {}", filename, error)
+            }
+            _ => {
+                panic!("Error opening file {}: {}", filename, error)
+            }
+        },
+    };
+
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
